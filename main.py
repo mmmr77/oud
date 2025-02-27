a2b12bdafce5bbee0a1a6d27e608010e6a0a97de
Mohammadmahdi Mahdavirad <mohammadmahdimahdavirad@gmail.com>
2025-02-27 20:48:13 +0330

    Add Sentry.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import logging
 
+import sentry_sdk
+
 from app import Application
 from config import settings
 from db_init import init_database
@@ -9,6 +11,11 @@ if __name__ == '__main__':
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
+    )
+
+    sentry_sdk.init(
+        dsn=f"https://{settings.SENTRY_TOKEN}@{settings.SENTRY_HOST}/8059",
+        traces_sample_rate=1.0,
     )
 
     if settings.DEBUG:
