02826183c5a1be5eb9c63e8635cf0504a5032334
Mohammadmahdi Mahdavirad <mohammadmahdimahdavirad@gmail.com>
2025-11-28 20:56:24 +0330

    Fix a bug in handling updates.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from telegram.ext import ApplicationBuilder, MessageHandler, filters, CommandHandler, \
-    CallbackQueryHandler, ConversationHandler
+from telegram.ext import (ApplicationBuilder, MessageHandler, filters, CommandHandler, CallbackQueryHandler,
+                          ConversationHandler)
 
 import const
 from admin import Admin
@@ -44,10 +44,46 @@ class Application:
 
         poem_handler = CallbackQueryHandler(Poem.show_poem_by_id, r'^poem:\d+$')
 
-        commands_handler = MessageHandler(filters.COMMAND, Command.general_command)
+        send_to_all_handler = ConversationHandler(entry_points=[CommandHandler('sendtoall', Command.sendtoall)],
+                                                  states={0: [MessageHandler(filters.TEXT, Admin.send_to_all)]},
+                                                  fallbacks=[])
 
-        recitation_saver_data_handler = MessageHandler(filters.TEXT & filters.ChatType.CHANNEL,
+        recitation_saver_data_handler = MessageHandler(filters.TEXT & filters.Chat(settings.OUD_FILES_CHANNEL_ID),
                                                        Recitation.add_recitation_data_to_db)
+
+        recitation_saver_audio_handler = MessageHandler(filters.AUDIO & filters.Chat(settings.OUD_FILES_CHANNEL_ID),
+                                                        Recitation.add_recitation_file_id_to_db)
+
+        search_query_handler = CallbackQueryHandler(Search.search_poems, r'^srch:.+:\d+$')
+
+        recitation_handler = CallbackQueryHandler(Recitation.get_recitation_by_id, r'^recitation:\d+$')
+
+        favorite_add_handler = CallbackQueryHandler(Favorite.add_to_favorites, r'^favadd:\d+$')
+
+        favorite_remove_handler = CallbackQueryHandler(Favorite.remove_from_favorites, r'^favremove:\d+$')
+
+        favorite_poems_handler = CommandHandler('favorites', Favorite.list_of_favorite_poems)
+
+        favorite_poems_query_handler = CallbackQueryHandler(Favorite.list_of_favorite_poems, r'^favorites:\d+$')
+
+        hafez_omen_intro_handler = CommandHandler('omen', Omen.show_omen_introduction)
+
+        hafez_show_omen_handler = CallbackQueryHandler(Omen.show_hafez_omen, r'^omen')
+
+        song_saver_data_handler = MessageHandler(filters.TEXT & filters.Chat(settings.OUD_MUSIC_CHANNEL_ID),
+                                                 Song.add_song_data_to_db)
+
+        song_saver_audio_handler = MessageHandler(filters.AUDIO & filters.Chat(settings.OUD_MUSIC_CHANNEL_ID),
+                                                  Song.add_song_file_id_to_db)
+
+        reply_opinion_handler = ConversationHandler(
+            entry_points=[MessageHandler(filters.Regex(r'^/reply_\d+_\d+$'), Opinion.save_user_info)],
+            states={0: [MessageHandler(filters.ALL & ~filters.Regex(rf'^{const.CANCEL}$'), Opinion.reply_to_opinion)]},
+            fallbacks=[MessageHandler(filters.Regex(rf'^{const.CANCEL}$'), Opinion.cancel)])
+
+        search_title_query_handler = CallbackQueryHandler(Search.search_title, r'^st:.+:\d+$')
+
+        commands_handler = MessageHandler(filters.COMMAND, Command.general_command)
 
         search_message_handler = ConversationHandler(
             entry_points=[MessageHandler(filters.TEXT, Search.search_destination)],
@@ -62,46 +98,10 @@ class Application:
             fallbacks=[MessageHandler(filters.Regex(rf"^{const.CANCEL}$"), Search.cancel)]
         )
 
-        search_query_handler = CallbackQueryHandler(Search.search_poems, r'^srch:.+:\d+$')
-
-        search_title_query_handler = CallbackQueryHandler(Search.search_title, r'^st:.+:\d+$')
-
-        recitation_saver_audio_handler = MessageHandler(filters.AUDIO & filters.ChatType.CHANNEL,
-                                                        Recitation.add_recitation_file_id_to_db)
-
-        recitation_handler = CallbackQueryHandler(Recitation.get_recitation_by_id, r'^recitation:\d+$')
-
-        send_to_all_handler = ConversationHandler(entry_points=[CommandHandler('sendtoall', Command.sendtoall)],
-                                                  states={0: [MessageHandler(filters.TEXT, Admin.send_to_all)]},
-                                                  fallbacks=[])
-
-        favorite_add_handler = CallbackQueryHandler(Favorite.add_to_favorites, r'^favadd:\d+$')
-
-        favorite_remove_handler = CallbackQueryHandler(Favorite.remove_from_favorites, r'^favremove:\d+$')
-
-        favorite_poems_handler = CommandHandler('favorites', Favorite.list_of_favorite_poems)
-
-        favorite_poems_query_handler = CallbackQueryHandler(Favorite.list_of_favorite_poems, r'^favorites:\d+$')
-
-        hafez_show_omen_handler = CallbackQueryHandler(Omen.show_hafez_omen, r'^omen')
-
-        hafez_omen_intro_handler = CommandHandler('omen', Omen.show_omen_introduction)
-
-        reply_opinion_handler = ConversationHandler(
-            entry_points=[MessageHandler(filters.Regex(r'^/reply_\d+_\d+$'), Opinion.save_user_info)],
-            states={0: [MessageHandler(filters.ALL & ~filters.Regex(rf'^{const.CANCEL}$'), Opinion.reply_to_opinion)]},
-            fallbacks=[MessageHandler(filters.Regex(rf'^{const.CANCEL}$'), Opinion.cancel)])
-
-        song_saver_data_handler = MessageHandler(filters.TEXT & filters.Chat(settings.OUD_MUSIC_CHANNEL_ID),
-                                                 Song.add_song_data_to_db)
-
-        song_saver_audio_handler = MessageHandler(filters.AUDIO & filters.Chat(settings.OUD_MUSIC_CHANNEL_ID),
-                                                  Song.add_song_file_id_to_db)
-
         self.application.add_handlers(
             [start_handler, opinion_handler, poets_handler, poet_details_handler, category_handler, poem_handler,
              send_to_all_handler, recitation_saver_data_handler, recitation_saver_audio_handler, search_query_handler,
              recitation_handler, favorite_add_handler, favorite_remove_handler, favorite_poems_handler,
-             favorite_poems_query_handler, hafez_omen_intro_handler, hafez_show_omen_handler, reply_opinion_handler,
-             song_saver_data_handler, song_saver_audio_handler, search_title_query_handler, commands_handler,
+             favorite_poems_query_handler, hafez_omen_intro_handler, hafez_show_omen_handler, song_saver_data_handler,
+             song_saver_audio_handler, reply_opinion_handler, search_title_query_handler, commands_handler,
              search_message_handler])
--- recitation.py
+++ recitation.py
@@ -1,35 +1,22 @@
-import functools
 import json
 
 from telegram import Update, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
-from config import settings
 from db import DataBase
 from util import Util
 
 RECITATION_TYPE = {0: 'ساده', 1: 'تفسیر'}
 
 
-def oud_files(func):
-    @functools.wraps(func)
-    async def wrapper_oud_files(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        if update.effective_chat.id == settings.OUD_FILES_CHANNEL_ID:
-            await func(update, context)
-
-    return wrapper_oud_files
-
-
 class Recitation:
     @staticmethod
-    @oud_files
     async def add_recitation_file_id_to_db(update: Update, context: ContextTypes.DEFAULT_TYPE):
         file_id = update.channel_post.audio.file_id
         recitation_id = int(update.channel_post.caption)
         DataBase().add_recitation_file_id(file_id, recitation_id)
 
     @staticmethod
-    @oud_files
     async def add_recitation_data_to_db(update: Update, context: ContextTypes.DEFAULT_TYPE):
         text = update.channel_post.text
         recitation_info = json.loads(text)
